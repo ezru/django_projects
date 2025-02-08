66ce36e44bb12c1fa6b06e1bc7918ea24c121fd2
ezru <saint.ezru@gmail.com>
2025-02-08 23:47:08 +0100

    modified view for index

--- itsmakertime/mekina/views.py
+++ itsmakertime/mekina/views.py
@@ -3,7 +3,10 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. Meet Mekina. The world's first fully 3D printable buggy robot!")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+    #return HttpResponse("Hello, world. Meet Mekina. The world's first fully 3D printable buggy robot!")
 
 def detail(request, question_id):
     return HttpResponse("You are looking at question %s." % question_id)
